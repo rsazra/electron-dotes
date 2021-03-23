37bb68dc96db9665402c3fb8e24c307e1111a5dd
rsazra <rajbirsinghazra@gmail.com>
2021-03-22 23:40:26 -0400

    test commit 2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
-
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'images/logo.png')
   });
 
   // and load the index.html of the app.
