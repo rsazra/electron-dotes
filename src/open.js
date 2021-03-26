ea7e5ad2a679032b2eaa1a0f67452ed7ac2abc9a
rsazra <rajbir.singh.azra@gmail.com>
2021-03-25 22:30:49 -0400

    change how file is made, add time on each open, and show file in textarea

--- src/open.js
+++ src/open.js
@@ -3,28 +3,31 @@ const path = require('path');
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
+const text = document.getElementById("content");
+
 const d = new Date();
+const time = `${d.getHours()}:${d.getMinutes()}:${String(d.getSeconds()).padStart(2,'0')}`
 const today = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`;
-const text = document.getElementById("fulltext");
+
 const journal = '/Users/rajbirsinghazra/Documents/journal';
 const month = path.join(journal, String(d.getFullYear()), months[d.getMonth()]);
 const file = path.join(month, today);
 
+let existed = true;
+
 fs.mkdirSync(month, { recursive: true });
 try {
     fs.accessSync(file);
-    console.log("in try");
 }
 catch (err) { // just assumes that the error is because file doesn't exist
-    const head = `${today}\n${d.toDateString()}`;
-    fs.writeFileSync(file, head);
-    console.log("caught");
+    existed = false;
 }
-console.log("continued");
 
-const time = `${d.getHours()}:${d.getMinutes()}:${String(d.getSeconds()).padStart(2,'0')}`
-// text.innerHTML = head;
-
+if (!existed) {
+    fs.writeFileSync(file, `${today}\n${d.toDateString()}`);
+}
+fs.appendFileSync(file, `\n\n${time} -- `);
+text.value = fs.readFileSync(file, { encoding: "utf8" });
 
 text.focus();
 text.selectionStart = text.value.length;
