2a248b9f87157a2192cd0caf40c18b6c500eff7f
rsazra <rajbir.singh.azra@gmail.com>
2021-05-08 18:11:26 -0400

    add function to date picker

--- src/alt.js
+++ src/alt.js
@@ -11,24 +11,9 @@ const yearselect = document.getElementById("year-picker");
 const monthselect = document.getElementById("month-picker");
 const dayselect = document.getElementById("day-picker");
 
-// ASSUMES LAST ENTRY IN ARRAY IS LATEST YEAR
 let curyear = years[years.length - 1];
-let yearfolder = path.join(journal, curyear);
-let monthindices = [];
-let curmonths = fs.readdirSync(yearfolder).filter((dirname) => {
-    if (months.indexOf(dirname) > -1) {
-        monthindices.push(months.indexOf(dirname));
-        return true;
-    }
-    return false;
-});
-let curmonth = months[Math.max.apply({}, monthindices)];
-let monthfolder = path.join(yearfolder, curmonth);
-let curdays = fs.readdirSync(monthfolder).filter((filename) => {return !filename.startsWith(".");});
-// ASSUMES LAST ENTRY IS LATEST DAY
-let startingfile = path.join(monthfolder, curdays[curdays.length - 1]);
-viewer.value = fs.readFileSync(startingfile, { encoding: "utf8" });
-
+let curmonth = "";
+let curfile = "";
 years.forEach((year) => {
     var option = document.createElement("option");
     option.text = year;
@@ -36,24 +21,81 @@ years.forEach((year) => {
     yearselect.add(option);
 });
 yearselect.value = curyear;
-curmonths.forEach((month) => {
+
+function updateMonths() {
+    let newyear = yearselect.value;
+    monthselect.innerHTML = "";
+    dayselect.innerHTML = "";
+
+    // ASSUMES LAST ENTRY IN ARRAY IS LATEST YEAR
+    let yearfolder = path.join(journal, newyear);
+    let monthindices = [];
+    let curmonths = fs.readdirSync(yearfolder).filter((dirname) => {
+        if (months.indexOf(dirname) > -1) {
+            monthindices.push(months.indexOf(dirname));
+            return true;
+        }
+        return false;
+    });
+    curmonth = months[Math.max.apply({}, monthindices)];
+
+    curmonths.forEach((month) => {
+        var option = document.createElement("option");
+        option.text = month;
+        option.value = month;
+        monthselect.add(option);
+    });
     var option = document.createElement("option");
-    option.text = month;
-    option.value = month;
+    option.text = "";
     monthselect.add(option);
-});
-monthselect.value = curmonth;
-curdays.forEach((day) => {
+    monthselect.value = "";
+
+    curyear = newyear;
+};
+
+function updateDays() {
+    let newmonth = monthselect.value;
+    if (newmonth == "") {
+        newmonth = curmonth;
+    }
+    dayselect.innerHTML = "";
+
+    let monthfolder = path.join(journal, curyear, newmonth);
+    let curdays = fs.readdirSync(monthfolder).filter((filename) => {return !filename.startsWith(".");});
+    // ASSUMES LAST ENTRY IS LATEST DAY
+    curfile = path.join(monthfolder, curdays[curdays.length - 1]);
+
+    curdays.forEach((day) => {
+        var option = document.createElement("option");
+        option.text = day.slice(-2);
+        option.value = day.slice(-2);
+        dayselect.add(option);
+    });
     var option = document.createElement("option");
-    option.text = day.slice(-2);
-    option.value = day.slice(-2);
+    option.text = "";
     dayselect.add(option);
-});
-dayselect.value = startingfile.slice(-2);
+    dayselect.value = "";
 
+    curmonth = newmonth;
+};
 
+function updateFile() {
+    let newday = `${curyear}-${String(months.indexOf(curmonth) + 1).padStart(2, '0')}-${dayselect.value}`;
+    if (newday == `${curyear}-${curmonth}-`) {
+        return;
+    }
+    newfile = path.join(journal, curyear, curmonth, newday);
+    viewer.value = fs.readFileSync(newfile, { encoding: "utf8" });
+    document.title = newday;
 
+}
 
-console.log(startingfile);
-console.log(curdays);
-document.title = "viewer";
+updateMonths();
+monthselect.value = curmonth;
+updateDays();
+dayselect.value = curfile.slice(-2);
+updateFile();
+
+yearselect.oninput = updateMonths;
+monthselect.oninput = updateDays;
+dayselect.oninput = updateFile;
