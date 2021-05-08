b0aff55b459025247071139324bfa1f68dd3a3f6
rsazra <rajbir.singh.azra@gmail.com>
2021-05-08 15:48:34 -0400

    update date picking on open

--- src/alt.js
+++ src/alt.js
@@ -7,30 +7,51 @@ const journal = config.get('journal')[0];
 const years = (fs.readdirSync(journal)).filter((dirname) => {return !isNaN(dirname);});
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 const viewer = document.getElementById("viewer");
+const yearselect = document.getElementById("year-picker");
+const monthselect = document.getElementById("month-picker");
+const dayselect = document.getElementById("day-picker");
 
-if (years.length == 0) {
-
-}
 // ASSUMES LAST ENTRY IN ARRAY IS LATEST YEAR
 let curyear = years[years.length - 1];
-let startingfile = path.join(journal, curyear);
+let yearfolder = path.join(journal, curyear);
 let monthindices = [];
-let curmonths = fs.readdirSync(startingfile).filter((dirname) => {
+let curmonths = fs.readdirSync(yearfolder).filter((dirname) => {
     if (months.indexOf(dirname) > -1) {
         monthindices.push(months.indexOf(dirname));
         return true;
     }
     return false;
 });
-if (curmonths.length == 0) {
-
-}
 let curmonth = months[Math.max.apply({}, monthindices)];
-startingfile = path.join(startingfile, curmonth);
-let curdays = fs.readdirSync(startingfile).filter((filename) => {return !filename.startsWith(".");});
+let monthfolder = path.join(yearfolder, curmonth);
+let curdays = fs.readdirSync(monthfolder).filter((filename) => {return !filename.startsWith(".");});
 // ASSUMES LAST ENTRY IS LATEST DAY
-startingfile = path.join(startingfile, curdays[curdays.length - 1]);
+let startingfile = path.join(monthfolder, curdays[curdays.length - 1]);
 viewer.value = fs.readFileSync(startingfile, { encoding: "utf8" });
+
+years.forEach((year) => {
+    var option = document.createElement("option");
+    option.text = year;
+    option.value = year;
+    yearselect.add(option);
+});
+yearselect.value = curyear;
+curmonths.forEach((month) => {
+    var option = document.createElement("option");
+    option.text = month;
+    option.value = month;
+    monthselect.add(option);
+});
+monthselect.value = curmonth;
+curdays.forEach((day) => {
+    var option = document.createElement("option");
+    option.text = day.slice(-2);
+    option.value = day.slice(-2);
+    dayselect.add(option);
+});
+dayselect.value = startingfile.slice(-2);
+
+
 
 
 console.log(startingfile);
